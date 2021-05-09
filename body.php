bac249d3fa97cd6602264f657227e4ef0a84aff2
Long Huỳnh <longhuynh@Longs-MacBook-Pro.local>
2021-05-09 23:18:07 +0700

    init

---
+++ body.php
@@ -0,0 +1,122 @@
+<div class="u-center-text u-margin-top-big">
+      <h2 id="about" class="heading-secondary">story about us</h2>
+    </div>
+    <div class="section-about">
+      <div class="about-box">
+        _
+        <div class="row">
+          <div class="col-1-of-2">
+            <div class="about-box__text">
+              <div class="about-box__text--title">
+                welcome to the LK company
+              </div>
+              <p class="about-box__text--content">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga
+                cumque autem officiis distinctio, voluptate qui minima. Magnam
+                tenetur suscipit eius beatae dolore nihil repudiandae atque
+                vero, rem, labore ullam ipsa incidunt enim id necessitatibus at
+                dolor! Incidunt sequi nihil amet.
+              </p>
+              <p class="about-box__text--content">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga
+                cumque autem officiis distinctio, voluptate qui minima. Magnam
+                tenetur suscipit eius beatae dolore nihil repudiandae atque
+                vero, rem, labore ullam ipsa incidunt enim id necessitatibus at
+                dolor! Incidunt sequi nihil amet.
+              </p>
+            </div>
+            <div class="btn-mobile">
+              <a href="#about" class="btn-mobile btn btn--blue"> read more </a>
+            </div>
+          </div>
+          <div class="col-1-of-2">
+            <div class="about-box__image">
+              <img
+                src="./img/about/1.png"
+                alt="about us"
+                class="about-box__image--1"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="u-center-text u-margin-top-big">
+      <h2 id="product" class="heading-secondary">our courses</h2>
+    </div>
+    <div class="section-products">
+      <div class="row">
+        <div class="col-1-of-2">
+          <div class="product__title">
+            <h1>Front-End Course</h1>
+          </div>
+          <div class="product__content">
+            <p>
+              Đến với lớp học bạn không chỉ được học kiến thức chuyên môn mà còn
+              được tham gia các buổi offline, giao lưu, phát triển kỹ năng mềm,
+              định hướng tương lai.
+            </p>
+          </div>
+          <div class="product__btn--side">
+            <a href="service.html">See More...</a>
+          </div>
+        </div>
+        <div class="col-1-of-2">
+          <div class="product__img--side">
+            <img src="./img/index/front.png" alt="" class="product__img--1" />
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-1-of-2">
+          <div class="product__title">
+            <h1>Back-End Course</h1>
+          </div>
+          <div class="product__content">
+            <p>
+              Đến với lớp học bạn không chỉ được học kiến thức chuyên môn mà còn
+              được tham gia các buổi offline, giao lưu, phát triển kỹ năng mềm,
+              định hướng tương lai.
+            </p>
+          </div>
+          <div class="product__btn--side">
+            <a href="service.html">See More...</a>
+          </div>
+        </div>
+        <div class="col-1-of-2">
+          <div class="product__img--side">
+            <img src="./img/index/backend.png" alt="" class="product__img--2" />
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-1-of-2">
+          <div class="product__title">
+            <h1>Algorithm Course</h1>
+          </div>
+          <div class="product__content">
+            <p>
+              Đến với lớp học bạn không chỉ được học kiến thức chuyên môn mà còn
+              được tham gia các buổi offline, giao lưu, phát triển kỹ năng mềm,
+              định hướng tương lai.
+            </p>
+          </div>
+          <div class="product__btn--side">
+            <a href="service.html">See More...</a>
+          </div>
+        </div>
+        <div class="col-1-of-2">
+          <div class="product__img--side">
+            <img src="./img/index/algor.png" alt="" class="product__img--3" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="u-center-text">
+      <h2 id="product" class="heading-secondary">Member of Course</h2>
+    </div>
+    <div class="section-member">
+      <div class="member__img--side">
+        <img src="./img/member/schools.jpg" alt="member" class="member__img" />
+      </div>
+    </div>
---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+      <p class="footer__text">Copyright © by LK 2021 | All Rights Reserved</p>
+    </footer>
+    <script
+      src="https://code.jquery.com/jquery-3.6.0.js"
+      integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+      crossorigin="anonymous"
+    ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="./js/script.js"></script>
+  </body>
+</html>
